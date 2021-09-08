b7366c4644ddf57e58ffbfaf0d8c7f51ce1d7c0b
unazed <leethax0r.coder@gmail.com>
2021-09-08 03:53:43 +0100

    design initial home template

--- html/events/home.js
+++ html/events/home.js
@@ -1,5 +1,24 @@
 (($$, ctx, current_ctx) => {
   if ( ctx !== $$.CTX.LOGIN && ctx !== $$.CTX.REGISTER )
   { return $$.assert_context(ctx, "login_or_register"); }
-  $("#login-container").html(/* TODO */).prop("id", "home-container");
+  $("#login-container").html(`
+<div class="d-flex flex-column">
+  <div class="d-flex" id="top-bar">
+    <p>top bar</p>
+  </div>
+  <div class="d-flex" id="control-container">
+    <div class="d-flex flex-column" id="control-bar">
+      <p>entry 1</p>
+      <p>entry 2</p>
+      <p>entry 3</p>
+    </div>
+    <div class="container border" id="control-body">
+      <h1>main container</h1>
+    </div>
+  </div>
+  <div class="d-flex" id="bottom-bar">
+    <p>bottom bar</p>
+  </div>
+</div>
+    `).prop("id", "home-container");
 })
